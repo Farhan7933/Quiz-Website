79f63b7979ace22392760af329f8ccd449df2eeb
unknown <alamfarhan389@gmail.com>
2022-03-16 23:40:03 +0530

    First_commit

---
+++ hello.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('.quiz-form');
+const ans = ["B", "B", "A", "A"];
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const cur = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    cur.forEach((option, idx) => {
+        if(option === ans[idx]) {
+            score += 25;
+        }
+    });
+
+    scrollTo(0, 0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score) {
+            clearInterval(timer);
+        }
+        output++;
+    }, 10);
+});
