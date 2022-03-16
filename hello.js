185874d516e35d7eb8fb77e62072ab000958b06c
unknown <alamfarhan389@gmail.com>
2022-03-17 00:32:49 +0530

    question_added

--- hello.js
+++ hello.js
@@ -1,15 +1,15 @@
 const form = document.querySelector('.quiz-form');
-const ans = ["B", "B", "A", "A"];
+const ans = ["B", "B", "A", "A", "B", "A", "B", "A", "B", "A"];
 const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
-    const cur = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    const cur = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
     cur.forEach((option, idx) => {
         if(option === ans[idx]) {
-            score += 25;
+            score += 10;
         }
     });
 
